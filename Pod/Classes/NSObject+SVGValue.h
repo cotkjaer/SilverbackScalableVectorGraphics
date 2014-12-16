143e2942503096866c2b5c0550ce6c987d30e50c
christian otkjær <christian.otkjaer@gmail.com>
2014-12-16 13:07:48 +0100

    Initial commit

---
+++ Pod/Classes/NSObject+SVGValue.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+SVGValue.h
+//  Pods
+//
+//  Created by Christian Otkjær on 16/12/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (SVGValue)
+
+@end
---
+++ Pod/Classes/NSObject+SVGValue.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+SVGValue.m
+//  Pods
+//
+//  Created by Christian Otkjær on 16/12/14.
+//
+//
+
+#import "NSObject+SVGValue.h"
+
+@implementation NSObject (SVGValue)
+
+@end
